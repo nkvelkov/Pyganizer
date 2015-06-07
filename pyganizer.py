87e0e3a5d571f03c13a3282231bf5d708fc81e35
nkvelkov <nikolai.velkov1@abv.bg>
2015-06-07 12:16:23 +0300

    Add implementation of the methods in the pyganizer class

--- pyganizer.py
+++ pyganizer.py
@@ -1,39 +1,55 @@
 import datetime
-
-
-class Event:
-    def __init__(self, datetime, name):
-        self.datetime = datetime
-        self.name = name
-
-
-class Task(Event):
-    def __init__(self, datetime, name, completeness):
-        self.datetime = datetime
-        self.name = name
-        self.completeness = completeness
+import time
 
 
 class Pyganizer:
     def __init__(self):
-        self.tasks = {}  # should be read from a file
+        self.todos = {}  # should be read from a file
 
     def execute(self):
-
         while True:
-            current_time = datetime.datetime.now()
+            now = datetime.datetime.now()
+            current_moment = (now.day, now.month, now.year)
 
-    def signal(self):
+            if self.tasks.get(current_moment) is not None:
+                signal(self.todos[current_moment])
+
+            time.sleep(1)
+
+    def signal(self, message):
+        print("{} {}".format(message[0], message[1]))
+
+    def add_event(self, date, name, message, alert_hours=1):
+        if alert_hours > 24:
+            target_moment = calculate_date(date, alert_hours)
+            self.todos[target_moment] = (name, message)
+        else:
+            target_moment = get_date(date)
+            self.todos[target_moment] = (name, message)
+
+    def calculate_date(self, date, alert_hours):
         pass
 
-    def add_event(self, date, hour, name, alert_hours=1):
-        pass
+    def add_task(self, date, hour, name, completeness, alert_hours=1):
+        if alert_hours > 24:
+            target_moment = calculate_date(date, alert_hours)
+            self.todos[target_moment] = (name, message, completeness)
+        else:
+            target_moment = get_date(date)
+            self.todos[target_moment] = (name, message, completeness)
 
-    def add_task(self, date, hour, name, alert_hours=1):
-        pass
+    def remove_event(self, name):
+        keys = todos.keys()
+        for key in keys:
+            if todos[key][0] is name:
+                todos.pop(key)
 
-    def remove_event(self, date, hour, name=""):
-        pass
+    def remove_task(self, name):
+        keys = todos.keys()
+        for key in keys:
+            if todos[key][0] is name:
+                todos.pop(key)
 
-    def remove_task(self, date, hour, name=""):
-        pass
+    def get_date(self, date):
+        return (date.day, date.month, date.year,
+                date.hour, date.minute, date.second)
