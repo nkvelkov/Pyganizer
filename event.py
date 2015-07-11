5a0e87cf454b058f73b3b65d0e962ddc575528e3
nkvelkov <nikolai.velkov1@abv.bg>
2015-07-11 11:26:30 +0300

    Add full implementation of the Pyganizer class

--- event.py
+++ event.py
@@ -1,5 +1,14 @@
+import arrow
 
 class Event:
-    def __init__(self, datetime, name):
-        self.datetime = datetime
+    def __init__(self, datetime, name, message, mode):
+        self.date = datetime
         self.name = name
+        self.message = message
+        self.mode = mode
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.message)
+
+    def to_string(self):
+        return "{} {}".format(str(self.date), self.name)
--- task.py
+++ task.py
@@ -1,8 +1,10 @@
-import event
+from event import Event
 
 
 class Task(Event):
-    def __init__(self, datetime, name, completeness):
-        self.datetime = datetime
+    def __init__(self, datetime, name, message, completeness, priority):
+        self.date = datetime
         self.name = name
+        self.message = message
         self.completeness = completeness
+        self.priority = priority
